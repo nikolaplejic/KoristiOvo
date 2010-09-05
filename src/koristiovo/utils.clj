c12dfb936d25c835afda76a453568951d8ed61a7
Nikola Plejic <nikola@plejic.com>
2010-09-05 16:57:26 +0200

    error handling for required fields + tests

--- src/koristiovo/utils.clj
+++ src/koristiovo/utils.clj
@@ -1,5 +1,32 @@
 (ns koristiovo.utils
-  (:require [net.cgrand.enlive-html :as html]))
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.contrib.error-kit :as kit])
+  (:use clojure.test))
 
 (defn render [t]
   (apply str t))
+
+;; ========================================
+;; Error checking
+;; ========================================
+
+(kit/deferror *required-field* [] [field]
+              "Indicates encouter of an empty required field"
+              {:msg (str "Empty field: " field)
+               :unhandled (kit/throw-msg Exception)}) 
+
+(defn check-empty
+  [field value]
+  (if (= value "")
+    (kit/raise *required-field* field)
+    true))
+
+;; ========================================
+;; Tests
+;; ========================================
+
+(deftest throws-exception-on-empty-field
+         (is (thrown-with-msg? Exception (re-pattern "Empty field\\: field") (check-empty "field" "")))) 
+
+(deftest does-not-throw-exception-on-non-empty-field
+         (is (= true (check-empty "field" "value")))) 
